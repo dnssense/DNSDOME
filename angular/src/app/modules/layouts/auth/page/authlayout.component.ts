5d7d72da1e89b1125379bbc914ab0afb832a1fd9
abdurrahman <abdurrahmanyildiz35@gmail.com>
2019-01-15 12:37:42 +0300

    lokalizasyon alt yapısı eklendi

--- angular/src/app/modules/layouts/auth/page/authlayout.component.ts
+++ angular/src/app/modules/layouts/auth/page/authlayout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/core/services/config.service';
+import { AlertService } from 'src/app/core/services/alert.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import { TranslatorService } from 'src/app/core/services/translator.service';
 
 @Component({
   selector: 'app-authlayout',
@@ -7,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private config: ConfigService, private notification:NotificationService, private translator:TranslatorService) { }
 
   ngOnInit() {
   }
 
+  language(lang: string) {
+    this.config.setTranslationLanguage(lang);
+  }
 }
