e85bb5e0263864049bb3c8a884351f8601fe4834
abdurrahman <abdurrahmanyildiz35@gmail.com>
2019-01-15 12:40:41 +0300

    environment değişkenleri eklendi

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -4,7 +4,11 @@
 
 export const environment = {
   production: false,
-  useMock:false
+  useMock:false,
+  BACKEND_API_ROOT_URL: 'http://localhost:8080/services',
+  API_CAPTCHA_KEY: '6LcjI3oUAAAAAAUW7egWmq0Q9dbLOcRPQUqcUD58',
+  COMPANY: 'roksit',
+  COMPANY_LOGO: 'roksit_logo_small.png',
 };
 
 /*
