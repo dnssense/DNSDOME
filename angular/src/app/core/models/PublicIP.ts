a9887269b8c0caa39504b9ccc66ad1d765bd5ad4
abdurrahman <abdurrahmanyildiz35@gmail.com>
2019-03-15 17:55:24 +0300

    logo type degistirildi

--- angular/src/app/core/models/PublicIP.ts
+++ angular/src/app/core/models/PublicIP.ts
@@ -12,7 +12,7 @@ export class PublicIP {
   public profile: DomainProfile;
   public bwList: BWList;
   public appUserProfile: ApplicationProfile;
-  public logo: string = '';
+  public logo: any;
   public blockMessage: string = '';
   public blockip: number[] = [];
   public ips: Array<string[]> = [];
