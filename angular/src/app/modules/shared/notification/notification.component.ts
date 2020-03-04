73347d8e8fd5fb8e6b56d2ad96f1a316edf2f7de
Cihan Çelen <cihan.celen37@hotmail.com>
2020-03-04 15:57:16 +0300

    notification close button

--- angular/src/app/modules/shared/notification/notification.component.ts
+++ angular/src/app/modules/shared/notification/notification.component.ts
@@ -13,7 +13,7 @@ export class NotificationComponent implements OnInit {
   constructor(private notificationService: NotificationService) {
     this.notificationService.getMessage().subscribe(x => {
       if (x && x.type && x.text) {
-      this.showNotification('top', 'right', x.type, x.text);
+        this.showNotification('top', 'right', x.type, x.text);
       }
     });
   }
@@ -26,26 +26,26 @@ export class NotificationComponent implements OnInit {
     const color = Math.floor((Math.random() * 6) + 1);
 
     $.notify({
-        icon: 'notifications',
-        message: msg
+      icon: 'notifications',
+      message: msg
     }, {
-        type: which,
-        timer: 2000,
-        placement: {
-            from: from,
-            align: align
-        },
-        template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0} alert-with-icon" role="alert">' +
-          '<button mat-raised-button type="button" aria-hidden="true" class="close" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-          '<i class="material-icons" data-notify="icon">notifications</i> ' +
-          '<span data-notify="title">{1}</span> ' +
-          '<span data-notify="message">{2}</span>' +
-          '<div class="progress" data-notify="progressbar">' +
-            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-          '</div>' +
-          '<a href="{3}" target="{4}" data-notify="url"></a>' +
+      type: which,
+      timer: 2000,
+      placement: {
+        from: from,
+        align: align
+      },
+      template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0} alert-with-icon" role="alert">' +
+        '<button mat-raised-button type="button" aria-hidden="true" class="close" data-notify="dismiss"> <svg style="position: relative; top: -3px" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg> </button>' +
+        '<i class="material-icons" data-notify="icon">notifications</i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
         '</div>'
     });
-}
+  }
 
 }
