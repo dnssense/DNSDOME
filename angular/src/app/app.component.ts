42a28750659b086503b08d955e5417afcc8d3adc
Cihan Çelen <cihan.celen37@hotmail.com>
2020-03-27 01:16:43 +0300

    renk güncellemsi yapıldı.

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { ConfigService, ConfigHost } from './core/services/config.service';
 import { AuthenticationService } from './core/services/authentication.service';
-import { RkUtilityService } from 'roksit-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
-  providers : []
+  providers: []
 })
 export class AppComponent {
+
   host: ConfigHost;
   title?: string;
   iconImage?: string;
+
   constructor(
     private config: ConfigService,
     private authenticationService: AuthenticationService,
-    utilityService: RkUtilityService,
-    private configService: ConfigService) {
+    private configService: ConfigService
+  ) {
     this.config.init();
     this.authenticationService.checkSessionIsValid();
     this.host = this.configService.host;
