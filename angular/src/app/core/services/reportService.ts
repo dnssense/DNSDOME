28c053805e5e8eb87b54f9867e8e59718aea3e98
Erkan Yüksel <erkan.yuksel@ykare.com>
2021-02-09 20:06:20 +0300

    [2.1.16] 'N/A' filtering

--- angular/src/app/core/services/reportService.ts
+++ angular/src/app/core/services/reportService.ts
@@ -48,19 +48,19 @@ export class ReportService {
       { 'name': 'domain', 'beautyName': 'TableColumn.Domain', 'hrType': '', 'aggsType': 'TERM', 'checked': false, placeholder: 'example.com or example.*', inputPattern: /^(\*?|\*?([.-]?\w+[.-]?)+\*?)$/ },
       { 'name': 'subdomain', 'beautyName': 'TableColumn.Subdomain', 'hrType': '', 'aggsType': 'TERM', 'checked': true, placeholder: 'example.com or example.*', inputPattern: /^(\*?|\*?([.-]?\w+[.-]?)+\*?)$/ },
       { 'name': 'sourceIp', 'beautyName': 'TableColumn.SourceIp', 'hrType': '', 'aggsType': 'TERM', 'checked': true, placeholder: '10.11.12.13 or 10.11.*', inputPattern: /^(\*?[012]?\d{1,2}\*?|\*?([012]?\d{1,2}\.){1,3}\*?|\*?([012]?\d{1,2}\.){1,3}[012]?\d{1,2}\*?)$/ },
-      { 'name': 'sourceIpCountryCode', 'beautyName': 'TableColumn.SourceCountry', 'hrType': 'COUNTRY_FLAG', 'aggsType': 'TERM', 'checked': false, inputPattern: /^([A-Z]*)$/i },
-      { 'name': 'destinationIp', 'beautyName': 'TableColumn.DestinationIp', 'hrType': '', 'aggsType': 'TERM', 'checked': true, placeholder: '10.11.12.13 or 10.11.*', inputPattern: /^(\*?[012]?\d{1,2}\*?|\*?([012]?\d{1,2}\.){1,3}\*?|\*?([012]?\d{1,2}\.){1,3}[012]?\d{1,2}\*?)$/ },
-      { 'name': 'destinationIpCountryCode', 'beautyName': 'TableColumn.DestinationCountry', 'hrType': 'COUNTRY_FLAG', 'aggsType': 'TERM', 'checked': false, inputPattern: /^([A-Z]*)$/i },
+      { 'name': 'sourceIpCountryCode', 'beautyName': 'TableColumn.SourceCountry', 'hrType': 'COUNTRY_FLAG', 'aggsType': 'TERM', 'checked': false, inputPattern: /^([A-Z]*|N\/?A?)$/i },
+      { 'name': 'destinationIp', 'beautyName': 'TableColumn.DestinationIp', 'hrType': '', 'aggsType': 'TERM', 'checked': true, placeholder: '10.11.12.13 or 10.11.*', inputPattern: /^(\*?[012]?\d{1,2}\*?|\*?([012]?\d{1,2}\.){1,3}\*?|\*?([012]?\d{1,2}\.){1,3}[012]?\d{1,2}\*?|N\/?A?)$/ },
+      { 'name': 'destinationIpCountryCode', 'beautyName': 'TableColumn.DestinationCountry', 'hrType': 'COUNTRY_FLAG', 'aggsType': 'TERM', 'checked': false, inputPattern: /^([A-Z]*|N\/?A?)$/i },
       { 'name': 'agentAlias', 'beautyName': 'TableColumn.AgentAlias', 'hrType': '', 'aggsType': 'TERM', 'checked': false },
       { 'name': 'userId', 'beautyName': 'TableColumn.UserId', 'hrType': '', 'aggsType': 'TERM', 'checked': false, hide: true },
       { 'name': 'action', 'beautyName': 'TableColumn.Action', 'hrType': 'DNS_ACTION', 'aggsType': 'TERM', 'checked': true, placeholder: 'Allow / Deny', inputPattern: /^(a|al|all|allo|allow|d|de|den|deny)$/i },
       { 'name': 'applicationName', 'beautyName': 'TableColumn.ApplicationName', 'hrType': '', 'aggsType': 'TERM', 'checked': false, inputPattern: /^[*a-zA-Z0-9_ \/]{0,32}$/i },
       { 'name': 'category', 'beautyName': 'TableColumn.Category', 'hrType': '', 'aggsType': 'TERM', 'checked': false, hide: true },
       { 'name': 'reasonType', 'beautyName': 'TableColumn.ReasonType', 'hrType': '', 'aggsType': 'TERM', 'checked': false },
-      { 'name': 'clientLocalIp', 'beautyName': 'TableColumn.ClientLocalIp', 'hrType': '', 'aggsType': 'TERM', 'checked': false, placeholder: '10.11.12.13 or 10.11.*', inputPattern: /^(\*?[012]?\d{1,2}\*?|\*?([012]?\d{1,2}\.){1,3}\*?|\*?([012]?\d{1,2}\.){1,3}[012]?\d{1,2}\*?)$/ },
-      { 'name': 'clientMacAddress', 'beautyName': 'TableColumn.ClientMacAddress', 'hrType': '', 'aggsType': 'TERM', 'checked': false, placeholder: '00:11:22:33:44:55 or 00:11:*', inputPattern: /^(\*?([0-9a-f]{1,2}?-?:?){1,6}\*?)$/ },
+      { 'name': 'clientLocalIp', 'beautyName': 'TableColumn.ClientLocalIp', 'hrType': '', 'aggsType': 'TERM', 'checked': false, placeholder: '10.11.12.13 or 10.11.*', inputPattern: /^(\*?[012]?\d{1,2}\*?|\*?([012]?\d{1,2}\.){1,3}\*?|\*?([012]?\d{1,2}\.){1,3}[012]?\d{1,2}\*?|N\/?A?)$/ },
+      { 'name': 'clientMacAddress', 'beautyName': 'TableColumn.ClientMacAddress', 'hrType': '', 'aggsType': 'TERM', 'checked': false, placeholder: '00:11:22:33:44:55 or 00:11:*', inputPattern: /^(\*?([0-9a-f]{1,2}?-?:?){1,6}\*?|N\/?A?)$/ },
       { 'name': 'clientBoxSerial', 'beautyName': 'TableColumn.ClientBoxSerial', 'hrType': '', 'aggsType': 'TERM', 'checked': false },
-      { 'name': 'hostName', 'beautyName': 'TableColumn.HostName', 'hrType': '', 'aggsType': 'TERM', 'checked': false, inputPattern: /^(\*?|\*?([.-]?\w+[.-]?)+\*?)$/ }
+      { 'name': 'hostName', 'beautyName': 'TableColumn.HostName', 'hrType': '', 'aggsType': 'TERM', 'checked': false, inputPattern: /^(\*?|\*?([.-]?\w+[.-]?)+\*?|N\/?A?)$/ }
     ]);
   }
 
